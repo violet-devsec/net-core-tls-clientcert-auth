88b6675f2cf40e7a668b66b4b90666a78cabc1a7
violet-devsec <titto.tvm@hotmail.com>
2022-09-05 11:36:06 +0530

    subject name validation added

--- ClientCertAuth.WebApi/Helpers/CertificateValidator.cs
+++ ClientCertAuth.WebApi/Helpers/CertificateValidator.cs
@@ -10,7 +10,7 @@ namespace ClientCertAuth.WebApi.Helpers
         {
             byte[] clientCertBytes = Convert.FromBase64String(cert[0]);
             using X509Certificate2 clientCert = new X509Certificate2(clientCertBytes);
-            if (clientCert.Thumbprint != "2E6E459C1A8BC6348B040CD61C809ED159835B0C")
+            if (clientCert.Thumbprint.Trim().ToUpper() != "2E6E459C1A8BC6348B040CD61C809ED159835B0C")
             {
                 return false;
             }
@@ -20,6 +20,21 @@ namespace ClientCertAuth.WebApi.Helpers
             {
                 return false;
             }
+
+            // 2. Check subject name of certificate
+            bool foundSubject = false;
+            string[] certSubjectData = clientCert.Subject.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string s in certSubjectData)
+            {
+                if (String.Compare(s.Trim(), "CN=<expected_subject_name>") == 0)
+                {
+                    foundSubject = true;
+                    break;
+                }
+            }
+            if (!foundSubject) return false;
+
+            string[] certIssuerData = clientCert.Issuer.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
 
             return true;
         }
